c18adc866e40303d582d79c4076555bd89f38286
Nicholas Peretti <peretti.nicholas@gmail.com>
2018-08-06 17:08:12 +0200

    Minor Style Fix

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -9,18 +9,21 @@ export default ({ todo, onComplete, index }) => {
       className="todo-item"
       onClick={toggle}
     >
-      <input type="checkbox" checked={checked} />
+      <div style={{width: '50px', verticalAlign: 'top'}}>
+        <input type="checkbox" checked={checked}/>
+      </div>
+      <div style={{width: '450px'}}>
       {checked ?
         <span
           style={{
-            color: 'grey',
+            color: '#BBB',
             textDecoration: 'line-through'
           }}
         >{
           text}
         </span>
       : text}
-      <br />
+      </div>
     </li>
   );
 };
